3789403070e9d78aea88ea3bb3bcc4a9bbd51d9d
Matt <matsarj@gmail.com>
2018-12-02 19:43:45 -0800

    Add error check to extract script

--- California Voter Registration/Extract.py
+++ California Voter Registration/Extract.py
@@ -1,42 +1,66 @@
 from bs4 import BeautifulSoup
 import urllib.request
+from urllib.error import URLError, HTTPError
 import os
 from datetime import datetime
 
-site = 'https://www.sos.ca.gov'
 
-resp = urllib.request.urlopen(site + '/elections/report-registration/')
-soup = BeautifulSoup(resp, from_encoding=resp.info().get_param('charset'))
-
-
-pages = {}
-for link in soup.find_all('a', href=True):
-    if '/elections/report-registration/' in link['href']:
-        pages[link.string[link.string.rfind('-') + 2:]] = site + link['href']
-
-
-files = {}
-for page_key, page_item in pages.items():
-    print(page_key)
-    resp = urllib.request.urlopen(page_item)
-    soup = BeautifulSoup(resp, from_encoding=resp.info().get_param('charset'))
-    for link in soup.find_all('a', href=True):
-        if 'xls' in link['href'] and 'politicalsub' in link['href']:
-            if page_key in files:
-                files[page_key].append(link['href'])
-            else:
-                files[page_key] = []
-                files[page_key].append(link['href'])
-
-directory = r'C:\Users\ASUS\voter_registration\California Voter Registration'
-
-
-for path_key, path in files.items():
-    for file in path:
-        last_slash = file.rfind('/')    
-        folder = datetime.strptime(path_key, '%B %d, %Y').strftime('%Y-%m-%d')
-        filename = directory + '\\' + folder + '\\' + file[last_slash + 1:]
-        
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        urllib.request.urlretrieve(file, filename)
+def extract_california(download_directory, date_after = '1-1-2001'):
+    """Scrapes voter registration data from CA Secretary of State site"""
     
+    try:
+        site = 'https://www.sos.ca.gov'
+        with urllib.request.urlopen(site + '/elections/report-registration/') as resp:
+            soup = BeautifulSoup(resp,'lxml')
+    except HTTPError as e:
+        print('Error Code: ', e.code)
+    except URLError as e:
+        print('Reason: ', e.reason)
+    
+    #Make a dictionary of all pages on the site which lead to voter files.
+    #Only include those dates after the date_after parameter.
+    pages = {}
+    for link in soup.find_all('a', href=True):
+        if '/elections/report-registration/' in link['href']:
+            date = link.string[link.string.rfind('-') + 2:]
+            if datetime.strptime(date, '%B %d, %Y') \
+                > datetime.strptime(date_after, '%m-%d-%Y'):
+                    pages[date] = site + link['href']
+            
+    #Make a dictionary of all excel files contained on each page
+    files = {}
+    for page_key, page_item in pages.items():
+        try:
+            with urllib.request.urlopen(page_item) as resp:
+                soup = BeautifulSoup(resp, 'lxml')
+        except HTTPError as e:
+            print('Error Code: ', e.code)
+        except URLError as e:
+            print('Reason: ', e.reason)
+        for link in soup.find_all('a', href=True):
+            if 'xls' in link['href']:
+                if page_key in files:
+                    files[page_key].append(link['href'])
+                else:
+                    files[page_key] = []
+                    files[page_key].append(link['href'])
+    
+    #Download all files into chosen directory
+    for path_key, path in files.items():
+        for file in path:
+            last_slash = file.rfind('/')    
+            folder = datetime.strptime(path_key, '%B %d, %Y').strftime('%Y-%m-%d')
+            filename = download_directory + '\\' + folder + '\\' + file[last_slash + 1:]
+            
+            try:
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+            except OSError:
+                pass
+            try:
+                urllib.request.urlretrieve(file, filename)
+            except HTTPError as e:
+                print('Error Code: ', e.code)
+            except URLError as e:
+                print('Reason: ', e.reason)
+                
+extract_california(r'C:\Users\ASUS\voter_registration\California Voter Registration', '1-1-2018')
--- California Voter Registration/Transform.py
+++ California Voter Registration/Transform.py
@@ -1,49 +1,54 @@
 import pandas as pd
 import os
-import shutil
+
+def transform(rootdir, date_start, date_end):
+    rootdir = r'C:\Users\ASUS\voter_registration\California Voter Registration'
+    all_years = pd.DataFrame()
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            if 'politicalsub' in os.path.join(subdir, file):
+                data = pd.read_excel(subdir + '\\' + file)
+                date = subdir[subdir.rfind('\\') + 1:]
+                fixed_path = rootdir + '\\fixed\\' + date + '_' + 'county_data.csv'
+                os.makedirs(os.path.dirname(fixed_path), exist_ok=True)
+                
+                
+                if data.index.nlevels == 3:
+                    data.reset_index(level = 1, drop = True, inplace = True)                    
+                
+                counties = [(ind, county) for ind, (county, district) in enumerate(data.index)
+                             if county == county and 
+                             all(x not in county for x in ['Total', 'District', 'Cities', 'Percent'])] 
+                
+                districts = [(ind, district) for ind, (county,district) in enumerate(data.index) 
+                            if district == district and 
+                            all(x not in district for x in ['District', 'Cities', 'Percent'])] 
+                
+                data = data.reset_index(drop = True)
+                
+                data = data.loc[[ind for (ind, district) in districts],]
+                
+                data['county'] = pd.cut(data.index, bins = [0] + [max(1, i) for i, x in counties] + [float('inf')], 
+                       labels = ['none'] +  [x for i, x in counties], include_lowest = True).tolist()
+                
+                data['district'] = [district for (ind, district) in districts]
+                data = data.reset_index(drop = True)
+                
+                types = ['County Supervisorial', 'US Congressional', 'State Senate', 
+                         'State Assembly', 'State Board of Equalization']
+                
+                district_type = list()
+                for place in data['district']:
+                    check = [t for t in types if t in place]
+                    district_type += check if check else ['City']
+                    
+                data['district_type'] = district_type
+                                
+                data['date'] = date
+                data.columns = ['_'.join(col.split()) for col in data.columns]
+                data.to_csv(fixed_path, index = False)
+                
+                all_years = pd.concat([all_years, data], sort = True)
+                
 
 
-rootdir = r'C:\Users\ASUS\voter_registration\California Voter Registration'
-
-data = pd.read_excel(r"C:\Users\ASUS\voter_registration\California Voter Registration\2012-04-06\politicalsub1.xls")
-df2 = data.reset_index(level = 1)
-
-counties = [(ind, county) for ind, (county, district, other) in enumerate(data.index)
-             if county == county and ('Total' not in county)]
-districts = [(ind, district) for ind, (county,district) in enumerate(data.index) 
-            if district == district and all(x not in district for x in ['District', 'Cities', 'Percent'])] 
-        
-
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-        if 'politicalsub' in os.path.join(subdir, file):
-            data = pd.read_excel(subdir + '\\' + file)
-            if data.index.nlevels == 3:
-                data.reset_index(level = 1, drop = True, inplace = True)                    
-            
-            fixed_path = rootdir + '\\fixed\\' + subdir[subdir.rfind('\\') + 1:] + '_' + 'county_data.csv'
-            os.makedirs(os.path.dirname(fixed_path), exist_ok=True)
-            
-            counties = [(ind, county) for ind, (county, district) in enumerate(data.index)
-                         if county == county and 
-                         all(x not in county for x in ['Total', 'District', 'Cities', 'Percent'])] 
-            
-            districts = [(ind, district) for ind, (county,district) in enumerate(data.index) 
-                        if district == district and 
-                        all(x not in district for x in ['District', 'Cities', 'Percent'])] 
-            
-            data = data.reset_index(drop = True)
-            
-            data = data.loc[[ind for (ind, district) in districts],]
-            
-            data['county'] = pd.cut(data.index, bins = [0] + [max(1, i) for i, x in counties] + [float('inf')], 
-                   labels = ['none'] +  [x for i, x in counties], include_lowest = True).tolist()
-            
-            data['district'] = [district for (ind, district) in districts]
-            data = data.reset_index(drop = True)
-            
-            data = data.set_index(['county', 'district']).unstack().reset_index().fillna(0)
-            data.columns = [y + '_' + x  for x, y in data.columns]
-            data.rename(columns={ data.columns[0]: 'county'}, inplace = True)
-    
-            data.to_csv(fixed_path, index = False)
